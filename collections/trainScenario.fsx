88eab47c5d8ed357e4a3e976279cce2958097fe0
Lawfty Goals <mymsnadressis@hotmail.com>
2026-02-28 20:41:45 +0100

    finish trainscenario

--- collections/trainScenario.fsx
+++ collections/trainScenario.fsx
@@ -1,3 +1,7 @@
+open System
+#r "nuget:FSToolkit.ErrorHandling, 2.13.0"
+open FsToolkit.ErrorHandling
+
 type PassengerClass =
     | FirstClass
     | SecondClass
@@ -16,18 +20,80 @@ type CarriageFeatures =
     | Quiet
     | Washroom
 
+type StopName = StopName of string
 
+type Stop =
+    { Name: StopName
+      TimeOfArrival: DateTime }
+
+type CarriageNumber = CarriageNumber of int
 
 type Carriage =
-    { Id: int
+    { CarriageNumber: CarriageNumber
       CarriageType: CarriageType
       Seats: int
-      CarriageFeatures: CarriageFeatures list }
+      CarriageFeatures: CarriageFeatures Set }
+
+
+type TrainId = TrainId of string
+
+type Train =
+    { Id: TrainId
+      Origin: Stop
+      Destination: Stop
+      Stops: Stop list
+      Exchange: Stop option
+      Carriages: Carriage list }
 
 let testCarriage =
-    { Id = 1
-      CarriageType = BuffetFood Hot
+    { CarriageNumber = CarriageNumber 1
+      CarriageType = BuffetFood HotAndCold
       Seats = 45
-      CarriageFeatures = [ WIFI; Quiet; Washroom ] }
+      CarriageFeatures = Set [ WIFI; Quiet; Washroom ] }
 
-printfn $"{testCarriage.CarriageType}"
+let testTrain =
+    { Id = TrainId "Train1"
+      Origin =
+        { Name = StopName "First"
+          TimeOfArrival = DateTime.Now }
+      Destination =
+        { Name = StopName "Last"
+          TimeOfArrival = DateTime.Now.AddHours(1) }
+      Stops = []
+      Exchange = None
+      Carriages = [ testCarriage; testCarriage ] }
+
+module TrainMethods =
+    let totalNumberOfSeats train =
+        (0, train.Carriages)
+        ||> List.fold (fun (accu: int) carriage -> accu + carriage.Seats)
+
+    let findStop stops (name: StopName) =
+        stops |> List.tryFind (fun stop -> stop.Name = name)
+
+    type TimeBetweenErrors =
+        | SameStartAndDestination of StopName
+        | NonExistentStop of StopName
+        | DestinationEarlierThanOrigin of StopName * StopName
+
+    let timeBetweenStops train start destination =
+        result {
+            if start = destination then
+                return! Error(SameStartAndDestination start)
+
+            let! starting = findStop train.Stops start |> Result.requireSome (NonExistentStop start)
+
+            let! ending =
+                findStop train.Stops destination
+                |> Result.requireSome (NonExistentStop destination)
+
+            if starting.TimeOfArrival > ending.TimeOfArrival then
+                return! Error(DestinationEarlierThanOrigin(destination, start))
+
+            return ending.TimeOfArrival - starting.TimeOfArrival
+
+        }
+
+    let findCarriagesWithFeature feature train =
+        train.Carriages
+        |> List.filter (fun carriage -> carriage.CarriageFeatures.Contains feature)
